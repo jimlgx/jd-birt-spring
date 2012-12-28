a0c9abeb5c09fc00ab219185a4aa13dfc59a8ee4
jimlgx <jimlgx@gmail.com>
2012-12-28 18:15:42 +0800

    添加pacakge-info.java

---
+++ src/test/java/com/jd/birt/spring/example/view/package-info.java
@@ -0,0 +1,16 @@
+/*
+ * @project jd-birt-spring
+ * @package com.jd.birt.spring.example.view
+ * @file package-info.java
+ * @version  1.0
+ * @author  wangjunming
+ * @time  2012-12-24
+ */
+/**
+ * <code>package-info</code>
+ * 
+ * @version  1.0
+ * @author  wangjunming
+ * @since 1.0  2012-12-24
+ */
+package com.jd.birt.spring.example.view;
